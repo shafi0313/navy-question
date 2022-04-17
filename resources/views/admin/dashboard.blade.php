7c3e0d8cafc117a5f73ceaf7ac7e3acd5fc25d7d
shafi97 <msh.shafiul@gmail.com>
2022-04-17 14:32:36 +0600

    re

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
 @php $m='dashboard'; $sm=''; $ssm=''; @endphp
 <div class="main-panel">
     <div class="content">
-        <div class="panel-header bg-primary-gradient">
+        {{-- <div class="panel-header bg-primary-gradient">
             <div class="page-inner py-5">
                 <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
                     <div>
@@ -17,8 +17,8 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="page-inner mt-5">
+        </div> --}}
+        {{-- <div class="page-inner mt-5">
             <div class="row mt-2">
                 <div class="col-md-6">
                     <div class="card full-height">
@@ -67,7 +67,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
     @include('admin.layout.footer')
 </div>
