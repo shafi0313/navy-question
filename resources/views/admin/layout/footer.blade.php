b4bd77107044b4047dfefadde8821a89b9f044c7
shafi97 <msh.shafiul@gmail.com>
2022-07-27 09:02:34 +0600

    logo

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -2,25 +2,13 @@
     <div class="container-fluid">
         <nav class="pull-left">
             <ul class="nav">
-                {{-- <li class="nav-item">
-                    <a class="nav-link" href="">
-
-                    </a>
-                </li> --}}
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        Help
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        Licenses
-                    </a>
+                    Bangladesh Navy &copy; {{date('Y')}} All Rights Reserved 
                 </li>
             </ul>
         </nav>
         <div class="copyright ml-auto">
-            {{date('Y')}}, Developed <i class="fa fa-heart heart text-danger"></i> by <a href="http://www.lscominc.com/">LS Communication</a>
+            {{date('Y')}}, Developed  by <a href="http://www.lscominc.com/"><img src="{{ asset('uploads/images/icons/ls.jpeg') }}" height="30px" alt=""></a>
         </div>
     </div>
 </footer>
