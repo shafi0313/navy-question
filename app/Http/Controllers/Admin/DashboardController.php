524a2faa0f1da493c4376e66df591e3c4c5940d9
shafi97 <msh.shafiul@gmail.com>
2022-12-21 18:32:39 +0600

    dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Chapter;
+use App\Models\Exam;
+use App\Models\Question;
+use App\Models\Subject;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $examCount = Exam::count();
+        $subjectCount = Subject::count();
+        $chapterCount = Chapter::count();
+        $quesCount = Question::count();
+        $multipleQuesCount = Question::whereType('multiple_choice')->count();
+        $shortQuesCount = Question::whereType('short_question')->count();
+        $longQuesCount = Question::whereType('long_question')->count();
+        return view('admin.dashboard', compact('examCount','subjectCount','chapterCount','quesCount','multipleQuesCount','shortQuesCount','longQuesCount'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,48 +18,135 @@
                 </div>
             </div>
         </div> --}}
-        {{-- <div class="page-inner mt-5">
-            <div class="row mt-2">
-                <div class="col-md-6">
-                    <div class="card full-height">
-                        <div class="card-body">
-                            <div class="card-title">Overall statistics</div>
-                            <div class="card-category">Daily information about statistics in system</div>
-                            <div class="d-flex flex-wrap justify-content-around pb-2 pt-4">
-                                <div class="px-2 pb-2 pb-md-0 text-center">
-                                    <div id="circles-1"></div>
-                                    <h6 class="fw-bold mt-3 mb-0">New Users</h6>
+        <div class="page-inner mt-5">
+            <div class="row">
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-round">
+                        <div class="card-body ">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="flaticon-chart-pie text-warning"></i>
+                                    </div>
                                 </div>
-                                <div class="px-2 pb-2 pb-md-0 text-center">
-                                    <div id="circles-2"></div>
-                                    <h6 class="fw-bold mt-3 mb-0">Sales</h6>
-                                </div>
-                                <div class="px-2 pb-2 pb-md-0 text-center">
-                                    <div id="circles-3"></div>
-                                    <h6 class="fw-bold mt-3 mb-0">Subscribers</h6>
+                                <div class="col col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Total Exam</p>
+                                        <h4 class="card-title">{{ $examCount }}</h4>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="card full-height">
-                        <div class="card-body">
-                            <div class="card-title">Total income & spend statistics</div>
-                            <div class="row py-3">
-                                <div class="col-md-4 d-flex flex-column justify-content-around">
-                                    <div>
-                                        <h6 class="fw-bold text-uppercase text-success op-8">Total Income</h6>
-                                        <h3 class="fw-bold">$9.782</h3>
-                                    </div>
-                                    <div>
-                                        <h6 class="fw-bold text-uppercase text-danger op-8">Total Spend</h6>
-                                        <h3 class="fw-bold">$1,248</h3>
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-round">
+                        <div class="card-body ">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="fa-solid fa-book-journal-whills text-primary"></i>
                                     </div>
                                 </div>
-                                <div class="col-md-8">
-                                    <div id="chart-container">
-                                        <canvas id="totalIncomeChart"></canvas>
+                                <div class="col col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Total Subject</p>
+                                        <h4 class="card-title">{{ $subjectCount }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="fa-solid fa-book-open text-secondary"></i>
+                                    </div>
+                                </div>
+                                <div class="col col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Total Chapter</p>
+                                        <h4 class="card-title">{{ $chapterCount }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="fa-solid fa-file-circle-question text-success"></i>
+                                    </div>
+                                </div>
+                                <div class="col col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Total Question</p>
+                                        <h4 class="card-title">{{ $multipleQuesCount }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="fa-solid fa-circle-question text-info"></i>
+                                    </div>
+                                </div>
+                                <div class="col col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Multiple Choose Question</p>
+                                        <h4 class="card-title">{{ $multipleQuesCount }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="fa-solid fa-circle-question text-warning"></i>
+                                    </div>
+                                </div>
+                                <div class="col col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Short Question</p>
+                                        <h4 class="card-title">{{ $shortQuesCount }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
+                                        <i class="fa-solid fa-circle-question text-danger"></i>
+                                    </div>
+                                </div>
+                                <div class="col col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Long Question</p>
+                                        <h4 class="card-title">{{ $longQuesCount }}</h4>
                                     </div>
                                 </div>
                             </div>
@@ -67,7 +154,8 @@
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
+
     </div>
     @include('admin.layout.footer')
 </div>
