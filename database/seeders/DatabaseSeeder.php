d098f454dc79591e6658ce9028536721b8489155
shafi0313 <msh.shafiul@gmail.com>
2024-08-09 21:32:25 +0600

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             SubjectSeeder::class,
             QuestionSeeder::class,
             QuesOptionSeeder::class,
+            MarkDistributionSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/MarkDistributionSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MarkDistributionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $mark_distributions = array(
+            array('id' => '1','subject_id' => '1','multiple' => '20','sort' => '0','long' => '0','created_at' => '2024-08-09 18:27:15','updated_at' => '2024-08-09 18:27:15'),
+            array('id' => '2','subject_id' => '2','multiple' => '20','sort' => '0','long' => '0','created_at' => '2024-08-09 18:27:15','updated_at' => '2024-08-09 18:27:15'),
+            array('id' => '3','subject_id' => '3','multiple' => '20','sort' => '0','long' => '0','created_at' => '2024-08-09 18:27:15','updated_at' => '2024-08-09 21:26:40'),
+            array('id' => '4','subject_id' => '4','multiple' => '20','sort' => '0','long' => '0','created_at' => '2024-08-09 18:27:15','updated_at' => '2024-08-09 21:26:40'),
+            array('id' => '5','subject_id' => '5','multiple' => '20','sort' => '0','long' => '0','created_at' => '2024-08-09 18:27:15','updated_at' => '2024-08-09 21:26:40')
+          );
+          MarkDistribution::insert($mark_distributions)
+    }
+}
