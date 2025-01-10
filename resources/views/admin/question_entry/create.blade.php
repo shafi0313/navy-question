24305f505f639bdf6fb1627ec1e831ad42ccf917
Shafi <msh.shafiul@gmail.com>
2025-01-10 11:48:29 +0600

    Add 'Is Important' field and enhance option handling in question entry form

--- resources/views/admin/question_entry/create.blade.php
+++ resources/views/admin/question_entry/create.blade.php
@@ -83,7 +83,17 @@
                                                 @endif
                                             </div>
                                         </div> --}}
-                                        <div class="col-md-3">
+                                        
+                                        <div class="col-md-2">
+                                            <div class="form-group">
+                                                <label for="type">Is Important <span class="t_r">*</span></label>
+                                                <select name="important" class="form-control">
+                                                    <option value="0">No</option>
+                                                    <option value="1">Yes</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-2">
                                             <div class="form-group">
                                                 <label for="mark">Marks <span class="t_r">*</span></label>
                                                 <input type="number" name="mark" class="form-control" id="mark"
@@ -93,7 +103,7 @@
                                                 @endif
                                             </div>
                                         </div>
-                                        <div class="col-md-3">
+                                        <div class="col-md-2">
                                             <div class="form-group">
                                                 <label for="image">Image</label>
                                                 <input type="file" name="image" class="form-control" id="image">
@@ -114,28 +124,40 @@
 
                                     </div>
                                 </div>
-                                <div class="row quesTypeDiv ml-2">
-                                    <div class="col-md-6">
-                                        <div class="row">
-                                            <div class="form-group ">
-                                                <label>Option: </label>
-                                                <input type="text" name="option" id="option" class="form-control"
-                                                    style="width: 230%">
-                                            </div>
-                                            <div style="margin-top: 38px; margin-left:238px">
-                                                <button class="btn btn-success add-item" type="button">Add</button>
-                                            </div>
-                                        </div>
+                                <div class="row ml-2 justify-content-center">
+                                    <div class="col-md-8">
+                                        <table class="table table-bordered table-hover">
+                                            <tr>
+                                                <td>Option</td>
+                                                <td>Answer</td>
+                                                <td></td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <input type="text" id="option" class="form-control">
+                                                </td>
+                                                <td>
+                                                    <input type="checkbox" id="correct" class="form-check"
+                                                        style="display: inline-block">
+                                                    <label for="correct" class="form-label">Correct</label>
+                                                </td>
+                                                <td class="text-center">
+                                                    <button class="btn btn-success btn-sm add-item"
+                                                        type="button">Add</button>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        
                                         <table class="table table-bordered table-hover item_data_table">
                                             <thead>
                                                 <tr>
                                                     <th>SN</th>
                                                     <th>Option</th>
+                                                    <th>Correct</th>
                                                     <th width="50px"></th>
                                                 </tr>
                                             </thead>
-                                            <tbody>
-                                            </tbody>
+                                            <tbody></tbody>
                                         </table>
                                     </div>
                                 </div>
@@ -157,22 +179,42 @@
         @include('admin.question_entry.get-js')
         <script>
             $('.add-item').on('click', function() {
-                var option = $('#option').val();
-                if (option == '') {
+                const option = $('#option').val();
+                const isCorrect = $('#correct').prop('checked');
+                let correct, correctText;
+
+                if (isCorrect) {
+                    correct = 1;
+                    correctText = 'Yes';
+                } else {
+                    correct = 0;
+                    correctText = 'No';
+                }
+                if (option === '') {
                     alert('This option field is required');
                     $('#option').focus();
                     return false;
                 }
-                var html = '<tr>';
-                html += '<tr class="trData"><td class="serial"></td><td>' + option + '</td><td align="center">';
-                html += '<input type="hidden" name="option[]" value="' + option + '" />';
-                html += '<a class="item-delete" href="#"><i class="fas fa-trash"></i></a></td></tr>';
+
+                var html = `
+                    <tr class="trData">
+                        <td class="serial"></td>
+                        <td>${option}</td>
+                        <td>${correctText}</td>
+                        <td align="center">
+                            <input type="hidden" name="option[]" value="${option}" />
+                            <input type="hidden" name="correct[]" value="${correct}" />
+                            <a class="item-delete" href="#"><i class="fas fa-trash"></i></a>
+                        </td>
+                    </tr>
+                `;
+
                 toast('success', 'Added');
                 $('.item_data_table tbody').append(html);
                 $('#option').val('');
+                $('#correct').prop('checked', false);
                 serialMaintain();
             });
-
             $('.item_data_table').on('click', '.item-delete', function(e) {
                 var element = $(this).parents('tr');
                 element.remove();
@@ -216,7 +258,7 @@
             function clear() {
                 $("#questionArea").html('');
                 $("#ques").val('');
-                // $("#mark").val('');
+                $("#mark").val('');
                 $("#option").val('');
                 $("#image").val('');
                 $(".item_data_table tbody").empty();
