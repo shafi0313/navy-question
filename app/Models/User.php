8812f4ba1f655dc56369f3747f23e447e56edda7
Shafi <msh.shafiul@gmail.com>
2025-02-28 20:53:14 +0600

    Remove unused date field: eliminate 'ordered_at' from the User model for improved clarity and maintenance

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    protected $dates = ['ordered_at', 'created_at', 'updated_at'];
+    protected $dates = ['created_at', 'updated_at'];
 
     public function createdBy()
     {
